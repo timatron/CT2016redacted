578900614ce92ef4d23f4f4ef57c7a312b642777
timatron <timatron@gmail.com>
2016-10-14 17:09:56 -0400

    whoops

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-<?php echo "yes i know php"; ?>
+<?php
+$filenames = array(
+    'logo.png',
+    'draft.png'
+);
+?>
+<html><body>
+<div style="width:90%">
+
+<?php
+    foreach( $filenames as $filename ):
+?>
+    <img src="images/<?php echo $filename; ?>"><br><br>
+<?php
+    endforeach;
+?>
+
+</div>
+</body></html>
